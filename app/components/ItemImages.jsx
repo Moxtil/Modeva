2a2ac31c107a9665ec790e35e07e6de8554a1cd6
Moxtil <sang.j52016@gmail.com>
2025-06-04 14:16:23 +0300

    Last FR

--- app/components/ItemImages.jsx
+++ app/components/ItemImages.jsx
@@ -11,11 +11,11 @@ export default function ItemImages({ item }) {
         <AddToFavButton item={item} />
       </div>
       <div
-        className={`hidden sm:block absolute w-auto p-1 -translate-y-0.5 text-center ${
+        className={`absolute w-auto p-1 -translate-y-0.5 text-center ${
           item.availabilityStatus === "Out of Stock"
             ? "bg-red-500"
             : "bg-green-500"
-        } font-semibold text-sm text-white top-[16px] rounded-sm left-2`}
+        } font-semibold text-sm text-white top-[26px] rounded-sm left-2`}
       >
         {item.availabilityStatus === "Out of Stock"
           ? "Out Of Stock !"
@@ -29,7 +29,7 @@ export default function ItemImages({ item }) {
         className="w-full max-h-[500px]  object-contain max-w-[500px] "
         onClick={() => console.log(item.images)}
       />
-      <div className="flex justify-start items-center flex-wrap gap-4">
+      <div className="flex justify-center items-center flex-wrap gap-4">
         {item?.images?.map((im) => {
           return (
             <Image
@@ -38,7 +38,7 @@ export default function ItemImages({ item }) {
               alt={item.title}
               width={125}
               height={50}
-              className={`object-cover max-w-[125px] border-2 border-[#727272] opacity-80 p-2 ${
+              className={`object-cover max-w-[250px] border-2 border-[#727272] opacity-80 p-2 ${
                 im === mainImg
                   ? "opacity-100 border-2 border-[#222] rounded-sm shadow-sm shadow-[#333]"
                   : "opacity-60 cursor-pointer"
